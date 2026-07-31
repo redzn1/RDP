d7bc6e7201ad10e16865a4e7ca09dbdf2d995db8
redzn1 <redzznlife+redzn@gmail.com>
2026-08-01 01:21:12 +0700

    Optimize QEMU startup parameters in start.sh
    Reduced memory and CPU resources, removed unused options, and adjusted networking.

--- start.sh
+++ start.sh
@@ -1,22 +1,14 @@
 #!/bin/bash
 
-# Jalankan QEMU dengan RDP port forwarding
 qemu-system-x86_64 \
-    -m 2048 \
-    -smp 2 \
+    -m 1024 \
+    -smp 1 \
     -drive file=/disk.qcow2,format=qcow2 \
     -cdrom /windows.iso \
-    -drive file=/virtio.iso,index=1,media=cdrom \
     -boot d \
-    -netdev user,id=net0,hostfwd=tcp::3389-:3389,hostfwd=tcp::8006-:8006 \
+    -netdev user,id=net0,hostfwd=tcp::3389-:3389 \
     -device e1000,netdev=net0 \
     -vnc :0 \
-    -cpu host \
-    -enable-kvm \
-    -daemonize
+    -nographic
 
-# Tunggu QEMU jalan
-sleep 30
-
-# TCP Proxy ke port 3389
-exec socat TCP-LISTEN:3389,fork,reuseaddr TCP:localhost:3389
+# Ga pake socat, langsung forward
